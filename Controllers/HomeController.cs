16e70fa5791dda61bbb5754586fa2066d81c170b
TOMÁS KALINSCKY <47494132@ort>
2023-10-12 15:34:07 -0300

    model BD y controller index hechos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewBag.Series = BD.listarSeries();
         return View();
     }
 
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,3 +1,38 @@
 using System.Data.SqlClient;
 using Dapper;
 using System.Collections.Generic;
+
+namespace TP_10_Garbarino_Aboudara_Kalinscky
+
+public static class BD
+{
+    private static string _connectionString = @"Server=localhost; DataBase=BDSeries;Trusted_Connection=True;";
+    
+
+    public static list<Series> listarSeries() {
+        list<Series> series;
+         using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Series";
+            series = db.Query<Series>(sql).ToList();
+        }
+        return series;
+    }
+
+    private static list<Temporadas> listarTemporadas(int IdSerie) {
+        list<Temporadas> temporadas;
+         using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Temporadas where IdSerie = @pIdSerie";
+            temporadas = db.Query<Temporadas>(sql, new {pIdSerie = IdSerie}).ToList();
+        }
+        return temporadas;
+    }
+
+     private static list<Actores> listarActores(int IdSerie) {
+        list<Actores> actores;
+         using(SqlConnection db = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Actores where IdSerie = @pIdSerie";
+            actores = db.Query<Temporadas>(sql, new {pIdSerie = IdSerie}).ToList();
+        }
+        return actores;
+    }
+}
