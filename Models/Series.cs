92d2e7df796f2602a59ae719f56b9894d13ecdf4
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-12 15:38:27 -0300

    faf

--- Models/Series.cs
+++ Models/Series.cs
@@ -6,15 +6,16 @@ namespace TP10_Aboudara_Garbarino_Kalinscky.Models{
         public int AñoInicio {get;set;}
         public string Sinopsis {get;set;}
         public string ImagenSerie {get;set;}
-    }
+    
     public Series(){
 
     }
-    public Series(int IdSerie = 0, string Nombre = "", int AñoInicio = 0, string Sinopsis = "", string AñoInicio = ""){
+    public Series(int IdSerie = 0, string Nombre = "", int AñoInicio = 0, string Sinopsis = "", string ImagenSerie = ""){
         this.IdSerie = IdSerie;
         this.Nombre = Nombre;
         this.AñoInicio = AñoInicio;
         this.Sinopsis = Sinopsis;
         this.ImagenSerie = ImagenSerie;
+    }
     }
 }
