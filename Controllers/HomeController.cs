1b353a14544afa2bd5b64f6e17d94bd1bd745728
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-12 15:53:21 -0300

    fafas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -29,4 +29,7 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public Series VerDetalleSerieAjax(int IdSerie){
+        Serie = BD.Ver
+    }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,50 @@
-﻿
+﻿function MostrarTemporada(IDS) {
+    $.ajax({
+
+        type: 'POST',
+        dataType: 'JSON',
+        url: '/Home/Index',
+        data: {IDSerie: IDS},
+        success:
+            function (response){
+                $("#TituloTemporada").html("Nombre de la temporada: " + response.TituloTemporada);
+                $("#NumeroTemporada").html("Temporada N°: " +response.NumeroTemporada);
+            }
+
+    }
+    );
+}
+function MostrarActores(IDS) {
+    $.ajax({
+
+        type: 'POST',
+        dataType: 'JSON',
+        url: '/Home/Index',
+        data: {IDSerie: IDS},
+        success:
+            function (response){
+                $("#NombreActor").html("Nombre del actor " +response.Nombre);
+            }
+
+    }
+    );
+}
+function MostrarActores(IDS) {
+    $.ajax({
+
+        type: 'POST',
+        dataType: 'JSON',
+        url: '/Home/Index',
+        data: {IDSerie: IDS},
+        success:
+            function (response){
+                $("#NombreSerie").html("Nombre: " + response.Nombre);
+                $("#AñoInicio").html("Año de estreno: " +response.AñoInicio);
+                $("#Sinopsis").html("Sintesis: " + response.Sinopsis);
+                $("#ImagenSerie").attr("src","/"+response.Nombre);
+            }
+
+    }
+    );
+}
+
