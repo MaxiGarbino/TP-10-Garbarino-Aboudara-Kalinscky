331541b6572afae8bffcf15e764b8ff3a794f041
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-12 15:10:18 -0300

    model hechos

--- Models/Series.cs
+++ Models/Series.cs
@@ -4,6 +4,17 @@ namespace TP10_Aboudara_Garbarino_Kalinscky.Models{
         public int IdSerie {get;set;}
         public string Nombre {get;set;}
         public int AñoInicio {get;set;}
-        public string sinopsis {get;}
+        public string Sinopsis {get;set;}
+        public string ImagenSerie {get;set;}
+    }
+    public Series(){
+
+    }
+    public Series(int IdSerie = 0, string Nombre = "", int AñoInicio = 0, string Sinopsis = "", string AñoInicio = ""){
+        this.IdSerie = IdSerie;
+        this.Nombre = Nombre;
+        this.AñoInicio = AñoInicio;
+        this.Sinopsis = Sinopsis;
+        this.ImagenSerie = ImagenSerie;
     }
 }
